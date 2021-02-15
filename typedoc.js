d4ddf6c664e1c8ae286a07e05aaa932de0bbf864
taystack <tay.stack@gmail.com>
2021-02-14 17:20:40 -0800

    Minimal docs

---
+++ typedoc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "entryPoints": [
+    "./source/getters/index.ts",
+    "./source/equality/index.ts",
+    "./source/math/index.ts",
+    "./source/timeDelta/index.ts",
+    "./source/humanize/index.ts",
+    "./source/helpers/index.ts",
+    "./source/constants.ts",
+    "./source/types.ts",
+  ],
+  "out": "docs",
+  "theme": "minimal"
+}
